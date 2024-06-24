2bfaa43dfeab7b92921c5c7e43f09a8002f98a4d
Yashita <yashita151293@gmail.com>
2024-06-24 15:03:52 +0530

    Appbar logout function12

--- frontend/src/components/Avatar.jsx
+++ frontend/src/components/Avatar.jsx
@@ -2,6 +2,6 @@ export const Avatar = ({
     nameInitials
 })=>{
     return <div className=" bg-gray-400 rounded-full h-12 w-12 flex justify-center">
-    <button  title="Log Out" >{nameInitials}</button>
+    <button>{nameInitials}</button>
 </div>
 }
