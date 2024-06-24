b28b91a9285b0229826529ceaf1ca6b502c06f00
Yashita <yashita151293@gmail.com>
2024-06-24 16:26:39 +0530

    Appbar logout function19

--- frontend/src/components/AppBar.jsx
+++ frontend/src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { Avatar } from "./Avatar"
+import { Avatar } from "./Avatar";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -6,31 +6,38 @@ export const AppBar = ({
     label,
     greetMessage,
     nameInitials,
-}) =>{
-
+}) => {
     const [clicked, setClicked] = useState(false);
     const navigate = useNavigate();
 
     return (
         <div className="sticky top-0 z-50 bg-white border-b-2 px-8 py-2">
-            <div className="text-lg">
-                {label}
-            </div>
-            <div className="flex items-center">
-                <div className="px-4">
-                    {greetMessage}
-                </div> 
-                <div onClick={() => setClicked(prevClicked => !prevClicked)}>
-                    <Avatar nameInitials={nameInitials} />
+            <div className="flex justify-between items-center">
+                <div className="text-lg">
+                    {label}
                 </div>
-                <div className="flex justify-end relative">
-                {clicked? <button onClick={()=>{
-                    navigate('/signin');
-                    localStorage.clear();
-                    }} className="absolute top-0 p-2 text-lg font-semibold bg-slate-300 w-24 text-center mt-4">
-                            Log out
-                            </button>: null}
-                </div>                
+                <div className="flex items-center relative">
+                    <div className="px-4">
+                        {greetMessage}
+                    </div> 
+                    <div onClick={() => setClicked(prevClicked => !prevClicked)}>
+                        <Avatar nameInitials={nameInitials} />
+                    </div>
+                </div>
             </div>
+            {clicked && (
+                <div className="absolute left-1/2 transform -translate-x-1/2 mt-4 p-2 text-lg font-semibold bg-slate-300 w-24 text-center">
+                    <button
+                        onClick={() => {
+                            navigate('/signin');
+                            localStorage.clear();
+                        }}
+                        className="w-full"
+                    >
+                        Log out
+                    </button>
+                </div>
+            )}
         </div>
-)}
+    );
+};
