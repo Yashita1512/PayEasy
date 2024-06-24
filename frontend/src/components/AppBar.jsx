29b972beff121214f3d8551068e1ce4ff5fb8f1b
Yashita <yashita151293@gmail.com>
2024-06-24 14:26:58 +0530

    Appbar logout function6

--- frontend/src/components/AppBar.jsx
+++ frontend/src/components/AppBar.jsx
@@ -17,13 +17,14 @@ export const AppBar = ({
                 <div className="px-4">
                     {greetMessage}
                 </div> 
-                <button onClick={setClicked((prevClicked)=>!prevClicked)}><Avatar nameInitials={nameInitials}/></button>
+                <button onClick={setClicked((prevClicked)=>!prevClicked)}><Avatar nameInitials={nameInitials}/>
                 {clicked? <button onClick={()=>{
             navigate('/signin');
             localStorage.clear();
           }} className="absolute top-0 p-2 text-lg font-semibold bg-slate-300 w-24 text-center mt-4">
                     Log out
                   </button>: null}
+                  </button>
             </div>
     </div>
 }
