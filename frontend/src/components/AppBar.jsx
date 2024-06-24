9076d0c4d6e3299d5fd1c0c47bd4eec201e9606a
Yashita <yashita151293@gmail.com>
2024-06-24 15:00:56 +0530

    Appbar logout function12

--- frontend/src/components/AppBar.jsx
+++ frontend/src/components/AppBar.jsx
@@ -11,26 +11,25 @@ export const AppBar = ({
     const [clicked, setClicked] = useState(false);
     const navigate = useNavigate();
 
-    return <div className="flex justify-between shadow-md h-14 p-3">
-            <div className="text-lg">
-                {label}
+    return (
+    <div className="flex justify-between shadow-md h-14 p-3">
+        <div className="text-lg">
+            {label}
+        </div>
+        <div className="flex items-center">
+            <div className="px-4">
+                {greetMessage}
+            </div> 
+            <div onClick={() => setClicked(prevClicked => !prevClicked)}>
+                <Avatar nameInitials={nameInitials} logOut={handleLogOut}/>
             </div>
-            <div className="flex items-center">
-                <div className="px-4">
-                    {greetMessage}
-                </div> 
-                <div onClick={() => setClicked(prevClicked => !prevClicked)}>
-                    <Avatar nameInitials={nameInitials} logOut={handleLogOut}/>
-                </div>
-                    {clicked && <button onClick={()=>{
-                            navigate('/signin');
-                            localStorage.clear();
-                        }} className="absolute top-0 p-2 text-lg font-semibold bg-slate-300 w-24 text-center mt-4">
-                                    Log out
-                                </button>}
-                    </div> 
-                </div>
-            </div>
+            {clicked && <button onClick={()=>{
+                    navigate('/signin');
+                    localStorage.clear();
+                }} className="absolute top-0 p-2 text-lg font-semibold bg-slate-300 w-24 text-center mt-4">
+                            Log out
+                        </button>}
+        </div>
     </div>
-}
+)}
 
